3a2b2e931291506583dd55fbfba2eb21c9633375
TarshSeriy <tarsergey5@gmail.com>
2024-04-06 22:35:19 +0500

    task

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class Task4 {
+    public static int findFactorial(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        } if(n == 2){
+            return 2;
+        }
+        else{
+            int factorial = 1;
+            for(int i = 2; i <= n; i++){
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int number = scanner.nextInt();
+        int fact = findFactorial(number);
+        System.out.println(fact);
+        scanner.close();
+
+
+    }
 }
